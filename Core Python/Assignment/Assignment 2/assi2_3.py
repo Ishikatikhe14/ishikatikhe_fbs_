95b1684af12ec6feb6f371d31d7bbdad1ab281cf
Ishikatikhe14 <ishikatikhe@gmail.com>
2026-07-23 18:07:49 +0530

    Add files via upload

---
+++ Core Python/Assignment/Assignment 2/assi2_3.py
@@ -0,0 +1,11 @@
+#Convert distant given in feet and inches into meter and centimeter.
+
+feet = 90
+inches = 60
+
+# Convert to meters and centimeters
+meters = feet * 0.3048 + inches * 0.0254
+centimeters = meters * 100
+
+print(" meters is:", meters)
+print(" centimeters is:", centimeters)
