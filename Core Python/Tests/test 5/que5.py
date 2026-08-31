9f8184b41b2f4f658689611716e52c150f8d976b
Ishikatikhe14 <ishikatikhe@gmail.com>
2026-08-31 12:55:29 +0530

    Refactor union of two lists implementation
    Updated the implementation to find the union of two lists by concatenating them directly.

--- Core Python/Tests/test 5/que5.py
+++ Core Python/Tests/test 5/que5.py
@@ -1,12 +1,6 @@
 #5. Python Program to Find the Union of two Lists without using set concept. very easy
 
-list1 = [1, 2, 3, 4]
-list2 = [3, 4, 5, 6]
-
-union = list1.copy()
-
-for i in list2:
-    if i not in union:
-        union.append(i)
-
-print("Union:", union)
+l1=[1,2,3,4,5]
+l2=[6,7,8,9,10]
+l=l1+l2
+print(l)
